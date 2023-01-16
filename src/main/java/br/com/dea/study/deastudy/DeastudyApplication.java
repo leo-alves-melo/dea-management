f53ae5206a32ea9fb57b8422201662baedb39840
Leonardo Alves de Melo <leonardoalvesdemelo@Leonardos-MacBook-Pro.local>
2023-01-16 16:19:01 -0300

    Creating Spring Project

---
+++ src/main/java/br/com/dea/study/deastudy/DeastudyApplication.java
@@ -0,0 +1,13 @@
+package br.com.dea.study.deastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeastudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeastudyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/dea/study/deastudy/controller/HelloWorldController.java
@@ -0,0 +1,10 @@
+package br.com.dea.study.deastudy.controller;
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+public class HelloWorldController {
+
+
+}
+
