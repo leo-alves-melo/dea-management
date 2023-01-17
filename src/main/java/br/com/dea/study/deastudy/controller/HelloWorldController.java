57d0531e2a9d8507d8bd47f113abb72dfcf62562
Leonardo Alves de Melo <leonardoalvesdemelo@Leonardos-MacBook-Pro.local>
2023-01-17 08:31:52 -0300

    Changin hello world message and configuring main class

--- src/main/java/br/com/dea/study/deastudy/controller/HelloWorldController.java
+++ src/main/java/br/com/dea/study/deastudy/controller/HelloWorldController.java
@@ -1,10 +1,16 @@
 package br.com.dea.study.deastudy.controller;
 
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloWorldController {
 
-
+    @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
+    public ResponseEntity<String> getFactory() {
+        return ResponseEntity.ok("hello world, Leo!");
+    }
 }
 
